766d0b3d4e80758818af29cca07e485991dcb3e2
Rakesh Kusuma <rockykusuma@gmail.com>
2026-05-16 08:57:23 +0530

    🔖 chore: bump klinkmac cask to 1.1.1

--- Casks/klinkmac.rb
+++ Casks/klinkmac.rb
@@ -1,6 +1,6 @@
 cask "klinkmac" do
-  version "1.1.0"
-  sha256 "c58734878984c5a707488e4304f81b7a104f9efe61f39c7513a85f360b04809c"
+  version "1.1.1"
+  sha256 "bf78644270d0f3e5453db2a244abce7263eda519bcd04332e797507c208b234f"
 
   url "https://github.com/rockykusuma/klinkmac/releases/download/v#{version}/KlinkMac.dmg"
   name "KlinkMac"
